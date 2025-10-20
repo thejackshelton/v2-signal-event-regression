e5d95b95fd62559cae2ad0b55eb22207ce7aab13
thejackshelton <me@jackshelton.com>
2025-10-19 20:36:40 -0500

    remove indicator

--- src/components/checkbox/index.ts
+++ src/components/checkbox/index.ts
@@ -1,11 +1,9 @@
 import { CheckboxError } from "./checkbox-error";
-import { CheckboxIndicator } from "./checkbox-indicator";
 import { CheckboxRoot } from "./checkbox-root";
 import { CheckboxTrigger } from "./checkbox-trigger";
 
 export const Checkbox = {
   Root: CheckboxRoot,
   Trigger: CheckboxTrigger,
-  Indicator: CheckboxIndicator,
   Error: CheckboxError,
 };
--- src/components/checkbox/repro.tsx
+++ src/components/checkbox/repro.tsx
@@ -5,13 +5,13 @@ export default component$(() => {
   const isChecked = useSignal(false);
 
   return (
-    <Checkbox.Root bind:checked={isChecked}>
-      <Checkbox.Trigger class="size-10 bg-yellow-500 ui-checked:bg-red-500">
-        <Checkbox.Indicator class="checkbox-indicator">
-          Checked
-        </Checkbox.Indicator>
-      </Checkbox.Trigger>
-      {isChecked.value && <Checkbox.Error>Error</Checkbox.Error>}
-    </Checkbox.Root>
+    <>
+      <Checkbox.Root bind:checked={isChecked}>
+        <Checkbox.Trigger class="size-10 bg-yellow-500 ui-checked:bg-red-500" />
+        {isChecked.value && <Checkbox.Error>Error</Checkbox.Error>}
+      </Checkbox.Root>
+
+      <p>Is checked: {isChecked.value ? "true" : "false"}</p>
+    </>
   );
 });
